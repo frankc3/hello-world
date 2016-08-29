f53a0d87ff2a39ee873ff98b118e27bdbac086f4
Frank Cabuy <frank.cabuy@gmail.com>
2016-08-29 22:30:48 +0200

    add main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+
+
+/*-------------------------------------------*/
+/*         MAIN                              */
+/*-------------------------------------------*/
+int main( int argc, char * argv[])
+{
+    int rslt=0;
+    printf("Number of arguments %d\n",argc);
+    printf("argv[0] = %s\n",argv[0]);
+}
